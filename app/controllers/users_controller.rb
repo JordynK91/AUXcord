17014ad978b92e0d5cc6384690c7b8806391baba
JordynK91 <JordynK91@gmail.com>
2018-03-13 17:35:37 -0400

    make a new accout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,10 +14,28 @@ class UsersController < ApplicationController
 		    redirect_to "/users/#{user.id}"		
         end
     end    
-
+ 
 	def new
+	@user = User.new
 	end
 
+	def create
+	user = User.new(user_params)
+    if user.save
+      redirect_to "/users/#{user.id}"
+    else 
+      flash[:alert] = "Account Info Invalid. Please Try Again." 
+    end
+  end
+
+  def destroy 
+  	 @user = User.find(current_user.id)
+    if @user.destroy
+    redirect_to '/users/new'
+  end
+
+end
+	
 	private
     def user_params
     	params.require(:user).permit(:username, :password, :location, :name, :email, :faves, :bio,

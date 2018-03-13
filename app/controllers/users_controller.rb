28012930c623afcbf1041596d0e3bb44a7772c00
DorisCCLin <doriscclin@gmail.com>
2018-03-13 17:06:58 -0400

    user #show/#edit added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,26 @@
 class UsersController < ApplicationController
 	def show
 		@user= User.find_by_id(params[:id])
-
 	end 
+	
+	def update
+		user= User.find_by_id(params[:id])
+	    if(user.id == current_user.id)
+	       user.update(user_params)
+	       flash[:message] = 'updated successfully'
+           redirect_to "/users/#{user.id}"	
+		else
+		   flash[:message] = 'you are not the user'
+		    redirect_to "/users/#{user.id}"		
+        end
+    end    
 
-	def edit
+	def new
+	end
 
-	end 
-
-
-	def new 
+	private
+    def user_params
+    	params.require(:user).permit(:username, :password, :location, :name, :email, :faves, :bio,
+:achievements)
+    end 
 end

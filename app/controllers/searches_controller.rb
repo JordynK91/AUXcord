c9b347f5cd970815c0b2e4acfd731aa3aa8f36b8
DorisCCLin <doriscclin@gmail.com>
2018-03-14 15:54:57 -0400

    small tests

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,8 @@
 class SearchesController < ApplicationController
     def index
         puts if params[:id]
-        response = HTTParty.get("http://app.ticketmaster.com/discovery/v1/events.json?keyword=#{params[:id]}&apikey=#{ENV["API_KEY"]}", format: :plain)
+        response = HTTParty.get("https://app.ticketmaster.com/discovery/v2/events.json?apikey=#{ENV["API_KEY"]&keyword=New York", format: :plain)
+
         @results = JSON.parse response, symbolize_names: true
     end
 

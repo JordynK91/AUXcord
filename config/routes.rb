2656eb02c5473f2f88ab2e4b2560b413772c3366
Ty Thompson <tylermoko@gmail.com>
2018-03-14 14:36:07 -0400

    adding search bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/search', to: 'searches#index'
+  post '/search', to: 'searches#search'
+  get '/search/:id', to: 'searches#index'
 
 
 resources :users

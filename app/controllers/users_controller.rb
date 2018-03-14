ef7599690a0df07a151c26aadb64892223692606
JordynK91 <JordynK91@gmail.com>
2018-03-14 12:53:14 -0400

    tabs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,9 +33,11 @@ class UsersController < ApplicationController
     if @user.destroy
     redirect_to '/users/new'
   end
-
 end
-	
+
+
+
+
 	private
     def user_params
     	params.require(:user).permit(:username, :password, :location, :name, :email, :faves, :bio,

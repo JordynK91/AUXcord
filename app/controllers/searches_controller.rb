982c0bf14fc62676c88ce3c43a495b578bc15ace
DorisCCLin <doriscclin@gmail.com>
2018-03-14 11:18:54 -0400

    navbar added

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,4 +3,6 @@ class SearchesController < ApplicationController
         response = HTTParty.get("https://app.ticketmaster.com/discovery/v1/events.json?apikey=#{ENV["API_KEY"]}")
         @results = JSON.parse response.body
     end
+
+
 end

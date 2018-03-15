98061a4031589c0203c672f32a1ba80bfd46c3d9
Ty Thompson <tylermoko@gmail.com>
2018-03-15 14:53:40 -0400

    adding js and filtering by time

--- app/assets/javascripts/searches.js
+++ app/assets/javascripts/searches.js
@@ -1,11 +1,7 @@
-// startDateTime=2018-03-15T00:00:00
- $(document).ready(function() {
+$(document).on('turbolinks:load', function(){
     var dateField = document.getElementById('date');
     var dt = new Date();
     var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
     var startDateTime = dt.getFullYear().toString() + "-" + (dt.getMonth() + 1).toString() + "-" + dt.getDate().toString() + "T00:00:00Z";
     dateField.value= startDateTime;
-    console.log(startDateTime);
-    console.log("this is the dateField", dateField);
-    console.log(time, dt.getFullYear().toString());
 });

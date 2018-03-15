899eeae0e39fd44ca3ab4aaa6668b75dec56800a
Ty Thompson <tylermoko@gmail.com>
2018-03-15 14:39:11 -0400

    adding filter for date

--- app/assets/javascripts/searches.js
+++ app/assets/javascripts/searches.js
@@ -1,9 +1,11 @@
-
-
+// startDateTime=2018-03-15T00:00:00
  $(document).ready(function() {
+    var dateField = document.getElementById('date');
     var dt = new Date();
-var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
-console.log(time, dt);
+    var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+    var startDateTime = dt.getFullYear().toString() + "-" + (dt.getMonth() + 1).toString() + "-" + dt.getDate().toString() + "T00:00:00Z";
+    dateField.value= startDateTime;
+    console.log(startDateTime);
+    console.log("this is the dateField", dateField);
+    console.log(time, dt.getFullYear().toString());
 });
-  
-  

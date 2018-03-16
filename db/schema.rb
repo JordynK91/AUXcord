085978bb955c085d4e269c68afa31ab3513abe18
JordynK91 <JordynK91@gmail.com>
2018-03-16 12:20:05 -0400

    made archive database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315150525) do
+ActiveRecord::Schema.define(version: 20180316160557) do
+
+  create_table "archives", force: :cascade do |t|
+    t.string "api_id"
+    t.integer "user_id"
+    t.string "name"
+    t.string "img_url"
+    t.string "date"
+    t.string "time"
+    t.string "venue"
+    t.string "info"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "concerts", force: :cascade do |t|
     t.string "api_id"

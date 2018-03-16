a26d0ff2a1f65bba5aca7d170032e08c80aba45e
DorisCCLin <doriscclin@gmail.com>
2018-03-16 14:52:06 -0400

    auto pass to past

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,6 +6,8 @@ class SearchesController < ApplicationController
         # https://app.ticketmaster.com/discovery/v2/events?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&latlong=41.8786108,-87.6244330&radius=100&unit=miles&startDateTime=2017-07-08T00:00:00Z&size=300&page=2
     end
 
+
+
     def show
         @concerts = Concert.all
         @event_id = params[:id]

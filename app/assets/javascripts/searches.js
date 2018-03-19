e3ee645da726b76e1f3a88f3f2c3fe1b5a3754b8
Ty Thompson <tylermoko@gmail.com>
2018-03-19 15:35:01 -0400

    adding ability to search only by keyword

--- app/assets/javascripts/searches.js
+++ app/assets/javascripts/searches.js
@@ -1,9 +1,9 @@
 $(document).on('turbolinks:load', function(){
+    getLocation();
     var dateField = document.getElementById('date');
     var stateSelect = document.querySelector(".stateSelect");
     var searchField = document.getElementById('keyword').focus();
     var changeLocation = document.querySelector(".changeLocation");
-    stateSelect.style.display = "none";
     var locationField = document.getElementById('latlon');
     var dt = new Date();
     var month = preZero(dt.getMonth() + 1);
@@ -14,7 +14,7 @@ $(document).on('turbolinks:load', function(){
 
     changeLocation.addEventListener("click", function(){
         locationField.value = "";
-        stateSelect.style.display = "block";
+        stateSelect.style.display = "none";
         changeLocation.style.display = "none";
     });
 
@@ -29,7 +29,6 @@ $(document).on('turbolinks:load', function(){
         }
     }
 
-    
     function getLocation() {
         if (navigator.geolocation) {
          navigator.geolocation.getCurrentPosition(setPosition,handleError);
@@ -37,28 +36,33 @@ $(document).on('turbolinks:load', function(){
         console.log("getLocation ran");
     }
     function setPosition(position) {
+        curLocation = position.coords.latitude + "," + position.coords.longitude;
         console.log("setPosition ran")
         locationField.value = position.coords.latitude + "," + position.coords.longitude;
     }
     function handleError(error) {
         switch(error.code) {
             case error.PERMISSION_DENIED:
+            console.log(error.message);
             stateSelect.style.display = "block";
                 break;
             case error.POSITION_UNAVAILABLE:
+            console.log(error.message);
             stateSelect.style.display = "block";   
                 break;
             case error.TIMEOUT:
+            console.log(error.message);
             stateSelect.style.display = "block";    
                 break;
             case error.UNKNOWN_ERROR:
+            console.log(error.message);
             stateSelect.style.display = "block";    
                 break;
         }
     }
     
     //latlong=39.9525839,-75.1652215
-    getLocation();
+    
    
 });
 

ee5b6375ee07bde17d7a78aff2fa4d48cc7e4897
DorisCCLin <doriscclin@gmail.com>
2018-03-24 13:02:47 -0400

    fix4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,8 +64,9 @@ class UsersController < ApplicationController
      def redirect
          client = Signet::OAuth2::Client.new(client_options)
          # 
-         redirect_to client.authorization_uri.to_s
-         # "https://id.heroku.com/oauth/authorize?client_id=#{client_options[:client_id]}&response_type=code&scope=#{client_options[:scope]}&state={anti-forgery-token}"     
+         redirect_to "https://id.heroku.com/oauth/authorize?client_id=#{client_options[:client_id]}&response_type=code&scope=globle&state={anti-forgery-token}"
+         # client.authorization_uri.to_s
+         #      
  	end
  
  	
